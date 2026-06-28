2546a1485772046faaef6734374e939b0fd02971
Kurt Thomas <kcgonzales1@student.hau.edu.ph>
2026-06-29 02:26:43 +0800

    Add Projects section

---
+++ main/static/main/js/script.js
@@ -0,0 +1,21 @@
+// Fade Animation
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".fade").forEach(section=>{
+
+observer.observe(section);
+
+});
