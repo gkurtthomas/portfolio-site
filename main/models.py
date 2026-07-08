08062415613c2f0f5fe0f3c61af46d40a71de4d3
Kurt Thomas <kcgonzales1@student.hau.edu.ph>
2026-07-08 23:23:26 +0800

    Add project and personal information models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class PersonalInformation(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50)
+
+    summary = models.TextField()
+
+    contact_number = models.CharField(max_length=20)
+
+    email = models.EmailField()
+
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    tech_stack = models.CharField(max_length=200)
+
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.project_name
