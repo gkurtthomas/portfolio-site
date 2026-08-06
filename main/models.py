8b8a3ce6b345cec966f265e00c8a60c6f74616da
Kurt Thomas <kcgonzales1@student.hau.edu.ph>
2026-08-06 13:11:54 +0800

    Add inquiry and testimony models

--- main/models.py
+++ main/models.py
@@ -30,3 +30,27 @@ class Project(models.Model):
 
     def __str__(self):
         return self.project_name
+
+class Inquiry(models.Model):
+    first_name = models.CharField(max_length=50)
+
+    last_name = models.CharField(max_length=50)
+
+    contact_number = models.CharField(max_length=20)
+
+    email = models.EmailField()
+
+    address = models.CharField(max_length=200)
+
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Testimony(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    content = models.TextField()
+
+    def __str__(self):
+        return self.full_name
